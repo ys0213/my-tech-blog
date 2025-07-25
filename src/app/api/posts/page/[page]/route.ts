5a9c31d94c79156eb18d847398ef99851c785b8c
ys <ys@email.com>
2025-07-25 21:54:47 +0900

    fix code

--- src/app/api/posts/page/[page]/route.ts
+++ src/app/api/posts/page/[page]/route.ts
@@ -1,11 +1,10 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getSortedPostsData } from '@/lib/posts';
 
 const POSTS_PER_PAGE = 5;
 
-export async function GET(request: Request, { params }: { params: { page: string } }) {
-  const paramsData = await params;
-  const page = parseInt(paramsData.page, 10) || 1;
+export async function GET(req: NextRequest, context: { params: { page: string } }) {
+  const page = parseInt(context.params.page, 10) || 1;
   const allPosts = getSortedPostsData();
 
   const startIdx = (page - 1) * POSTS_PER_PAGE;
