41006e61b06c8e5079ee004b04d6ac97f4ab51b4
ys <ys@email.com>
2025-07-25 22:11:01 +0900

    5

--- src/app/api/posts/page/[page]/route.ts
+++ src/app/api/posts/page/[page]/route.ts
@@ -3,13 +3,21 @@ import { getSortedPostsData } from '@/lib/posts';
 
 const POSTS_PER_PAGE = 5;
 
-export async function GET(req: NextRequest, { params }: { params: { page: string } }) {
-  const page = parseInt(params.page, 10) || 1;
+type Context = {
+  params: {
+    page: string;
+  };
+};
+
+export async function GET(
+  req: NextRequest,
+  context: Context
+) {
+  const page = parseInt(context.params.page, 10) || 1;
   const allPosts = getSortedPostsData();
 
   const startIdx = (page - 1) * POSTS_PER_PAGE;
   const pagedPosts = allPosts.slice(startIdx, startIdx + POSTS_PER_PAGE);
-
   const totalPages = Math.ceil(allPosts.length / POSTS_PER_PAGE);
 
   return NextResponse.json({
