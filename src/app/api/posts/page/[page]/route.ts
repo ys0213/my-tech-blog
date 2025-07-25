08107190a090bdce24cd5781836bcb9475fb6516
ys <ys@email.com>
2025-07-25 22:15:03 +0900

    6

--- src/app/api/posts/page/[page]/route.ts
+++ src/app/api/posts/page/[page]/route.ts
@@ -2,18 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getSortedPostsData } from '@/lib/posts';
 
 const POSTS_PER_PAGE = 5;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export async function GET(req: NextRequest, context: any) {
+  const pageParam = context?.params?.page;
+  const page = pageParam ? parseInt(pageParam, 10) : 1;
 
-type Context = {
-  params: {
-    page: string;
-  };
-};
-
-export async function GET(
-  req: NextRequest,
-  context: Context
-) {
-  const page = parseInt(context.params.page, 10) || 1;
   const allPosts = getSortedPostsData();
 
   const startIdx = (page - 1) * POSTS_PER_PAGE;
